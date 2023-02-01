7567f15cd4080f40b219f60da485ec512d6fbe57
Sailabala-cell <sailabala.soren67@gmail.com>
2023-02-01 13:11:23 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,98 @@
-import React from 'react'
-import ChangeMode from "./ChangeMode"
+import React, {useState, useEffect,useRef} from "react"
 import "./App.css"
-function App() {
+import { lazy , Suspense} from "react";
+import FormInput from "./components/FormInput";
 
-  return (
-   <div style={{display:"flex", justifyContent:"center",alignItems:"center",height:"100vh",width:"100%"}}>
-    <ChangeMode/>
-   </div>
-  )
+// const Child= lazy(()=>import ('./Child'))
+// const ChildComponent = lazy(()=>import ('./ChildComponent'))
+export default function App() {
+const [values,setValues]= useState({
+  username:"",
+  email:"",
+  birthday:"",
+  password:"",
+  confirmpassword:""
+})
+ 
+const inputs = [
+  {
+    id: 1,
+    name: "username",
+    type: "text",
+    placeholder: "Username",
+    errorMessage:
+      "Username should be 3-16 characters and shouldn't include any special character!",
+    label: "Username",
+    pattern: "^[A-Za-z0-9]{3,16}$",
+    required: true,
+  },
+  {
+    id: 2,
+    name: "email",
+    type: "email",
+    placeholder: "Email",
+    errorMessage: "It should be a valid email address!",
+    label: "Email",
+    required: true,
+  },
+  {
+    id: 3,
+    name: "birthday",
+    type: "date",
+    placeholder: "Birthday",
+    label: "Birthday",
+  },
+  {
+    id: 4,
+    name: "password",
+    type: "password",
+    placeholder: "Password",
+    errorMessage:
+      "Password should be 8-20 characters and include at least 1 letter, 1 number and 1 special character!",
+    label: "Password",
+    pattern: `^(?=.*[0-9])(?=.*[a-zA-Z])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{8,20}$`,
+    required: true,
+  },
+  {
+    id: 5,
+    name: "confirmPassword",
+    type: "password",
+    placeholder: "Confirm Password",
+    errorMessage: "Passwords don't match!",
+    label: "Confirm Password",
+    pattern: values.password,
+    required: true,
+  },
+];
+
+const handleSubmit=(e)=>{
+  e.preventDefault()
+  const data=new FormData(e.target)
+  console.log(Object.fromEntries(data))
 }
 
-export default App
+const onChange=(e)=>{
+  setValues({...values, [e.target.name]:e.target.value})
+
+}
+
+console.log(values)
+  return (
+  <div className="app">
+ 
+  <form onSubmit={handleSubmit}>
+  <h1>Registartion</h1>
+    {inputs.map((input) => (
+      <FormInput 
+      key={input.id} 
+      {...input} 
+      value={values[input.name]}
+        onChange={onChange}
+      />
+    ))}
+     <button>Submit</button>
+  </form>
+  </div>
+)
+
+}
---
+++ src/components/FormInput.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import "./forminput.css"
+import { useState } from 'react'
+function FormInput(props) {
+    const [focused, setFocused] = useState(false)
+    const {label,errorMessage,onChange,id,...inputProps} = props
+
+
+    const handleFocus=(e)=>{
+        setFocused(true)
+    }
+  return (
+    <div className='formInput'>
+      <label>{label}</label>
+      <input 
+      {...inputProps} 
+      
+      onChange={onChange} 
+      
+      required 
+      onBlur={handleFocus} 
+      onFocus={() =>
+          inputProps.name === "confirmPassword" && setFocused(true)
+        }
+      focused={focused.toString()} />
+      <span>{errorMessage}</span>
+    </div>
+  )
+}
+
+export default FormInput
